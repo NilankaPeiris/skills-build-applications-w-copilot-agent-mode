82306037d33ab27a21e48b298615bce54e5ea427
Nilanka Peiris <51223410+NilankaPeiris@users.noreply.github.com>
2025-04-08 08:03:17 +0000

    Update database engine to use Djongo for MongoDB integration

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.dummy",  # Dummy engine since pymongo will be used directly
+        "ENGINE": "djongo",
         "NAME": "octofit_db",
         "HOST": "localhost",
         "PORT": 27017,
